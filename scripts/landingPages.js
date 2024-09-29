4bf3eca3619f3eae09ade7e9c952d8436b87c325
Festus <techarenaz01@gmail.com>
2024-09-28 22:25:20 -0700

    Landing Page Submenu update: Product and Solutions sub menu issue resolved

--- scripts/landingPages.js
+++ scripts/landingPages.js
@@ -1,5 +1,5 @@
 const hiddenMenu = document.getElementsByClassName("drop-down-menu-section")[0];
-const SolutionHiddenMenu = document.getElementsByClassName("solutions-down-menu-section")[1];
+const SolutionHiddenMenu = document.getElementsByClassName("drop-down-menu-section")[1];
 const productMenuButton = document.getElementById("product-menu-btn");
 const solutionMenuButton = document.getElementById ("solution-menu-btn");
 
@@ -10,15 +10,7 @@ function toggleSubMenu() {
     console.error('Anchor element is not defined');
   }
 }
-//this function is not working yet, try to fix when you wake up
-function toggleSolutionSubMenu() {
-  if (SolutionHiddenMenu !== null && SolutionHiddenMenu !== undefined) {
-    SolutionHiddenMenu.classList.toggle('show');
-  } else {
-    console.error('Anchor element is not defined');
-  }
-}
 
 productMenuButton.addEventListener('mouseover', toggleSubMenu);
 
-solutionMenuButton.addEventListener('click', toggleSolutionSubMenu);
+solutionMenuButton.addEventListener('click', toggleSubMenu);
